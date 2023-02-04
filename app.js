a29fe340f99224b25d2a48a554358177802e1ea2
Matthew English <m6english@gmail.com>
2023-02-04 16:08:03 -0500

    used js to indicate current day in modal

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const flip = document.getElementById('flip');
-const flipInner = document.getElementsByClassName('.')
+function currentDay() {
+    var d = new Date().getDay();
+    if ( d < 1 || d > 5) {
+    document.getElementById("day-"+d).classList.add('closed');
+    document.getElementById("day-"+d).classList.remove('line');
+    } else {
+        document.getElementById("day-"+d).classList.add('open');
+        document.getElementById("day-"+d).classList.remove('line');
+    }
+}
 
-
-flip.addEventListener('click', () => {
-
-});
+currentDay();
