f4d50b7832aa3b6c66d2c1c32abd502a6fd3b5bd
Matthew English <m6english@gmail.com>
2023-04-12 13:31:27 -0400

    Improved button functionality

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let flipCard = document.getElementsByClassName('flip-card');
 let flipCardInner = document.getElementsByClassName('flip-card-inner');
 let staffBtn = document.getElementsByClassName('staff-button');
+let backBtn = document.getElementsByClassName('back-button');
+let staffCard = document.getElementsByClassName('staff-card');
 
-let arr = [...flipCard];
+let arrBtn = [...staffBtn];
+let arrBack = [...backBtn];
 let arrFlip = [...flipCardInner];
+let arrCard = [...staffCard];
 
 function currentDay() {
     var d = new Date().getDay();
@@ -18,16 +22,16 @@ function currentDay() {
 
 currentDay();
 
-for (let i = 0; i < arr.length; i++) {
-    arr[i].addEventListener('click', e => {
+for (let i = 0; i < arrBtn.length; i++) {
+    arrBtn[i].addEventListener('click', e => {
         arrFlip[i].style.transform = 'rotateY(180deg)';
-        staffBtn[i].style.display = 'none';
+        arrBack[i].style.zIndex = '1';
+        arrBtn[i].style.zIndex = '-1';
+    });
+    arrBack[i].addEventListener('click', e => {
+        arrFlip[i].style.transform = 'rotateY(0deg)';
+        arrBack[i].style.zIndex = '-1';
+        arrBtn[i].style.zIndex = '1';
     })
 }
 
-for (let i = 0; i < arr.length; i++) {
-    arr[i].addEventListener('mouseleave', e => {
-        arrFlip[i].style.transform = 'rotateY(0deg)';
-        staffBtn[i].style.display = 'block';
-    })
-}
