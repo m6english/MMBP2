5b29d808868c1b2c8efba5254ff842321cd2b431
Matthew English <m6english@gmail.com>
2023-04-12 10:58:50 -0400

    reworked staff cards
    Added flip button and flip on click. This resolved the issue of cards not flipping properly on iOS mobile devices

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+let flipCard = document.getElementsByClassName('flip-card');
+let flipCardInner = document.getElementsByClassName('flip-card-inner');
+let staffBtn = document.getElementsByClassName('staff-button');
+
+let arr = [...flipCard];
+let arrFlip = [...flipCardInner];
+
 function currentDay() {
     var d = new Date().getDay();
-    if ( d < 1 || d > 5) {
-    document.getElementById("day-"+d).classList.add('closed');
-    document.getElementById("day-"+d).classList.remove('line');
+    if (d < 1 || d > 5) {
+        document.getElementById("day-" + d).classList.add('closed');
+        document.getElementById("day-" + d).classList.remove('line');
     } else {
-        document.getElementById("day-"+d).classList.add('open');
-        document.getElementById("day-"+d).classList.remove('line');
+        document.getElementById("day-" + d).classList.add('open');
+        document.getElementById("day-" + d).classList.remove('line');
     }
 }
 
 currentDay();
+
+for (let i = 0; i < arr.length; i++) {
+    arr[i].addEventListener('click', e => {
+        arrFlip[i].style.transform = 'rotateY(180deg)';
+        staffBtn[i].style.display = 'none';
+    })
+}
+
+for (let i = 0; i < arr.length; i++) {
+    arr[i].addEventListener('mouseleave', e => {
+        arrFlip[i].style.transform = 'rotateY(0deg)';
+        staffBtn[i].style.display = 'block';
+    })
+}
